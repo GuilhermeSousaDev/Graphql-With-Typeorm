2ddbffa697dd17babaf91ae0085facc168a9292f
GuilhermeSousaDev <a.guilhermerocha250@gmail.com>
2023-06-10 11:07:16 -0300

    Hashed the password

--- src/typeorm/services/User/CreateUserService.ts
+++ src/typeorm/services/User/CreateUserService.ts
@@ -1,11 +1,14 @@
 import { User } from "../../entities/User";
 import UserRepository from "../../repositories/UserRepository";
+import { hash } from "bcryptjs";
 
 export default class CreateUserService {
     public async execute(data: { name: string, email: string, password: string }): Promise<User> {
         const userRepository = new UserRepository();
 
         const user = await userRepository.create(data);
+
+        user.password = await hash(data.password, 8);
 
         await userRepository.save(user);
 
